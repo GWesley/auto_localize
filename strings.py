e9000e3346e1f292d59313ced6cafd48581f6cfd
Fahad <fahad@guidedways.com>
2021-11-07 13:06:39 +0400

    WIP - delta translations Cleanup

--- strings.py
+++ strings.py
@@ -8,10 +8,13 @@ translator = Translator()
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", default="Localizable.strings", help="set the path to the original Localizable.strings to read keys from")
 parser.add_argument("-o", default="en", help="set the origin locale for auto translation, default is english")
+parser.add_argument("-d", default="", help="For delta translations. Set the path to the root directory where existing localized translations exist. If specified, this path will be used to check if a line / key has already been translated and skip translating that line. This way only the keys that do not exist in the existing destination file will be translated.")
 parser.add_argument("-v", default="1", help="Verbose")
 args = parser.parse_args()
 
 # Read and cache origin language once
+print("Reading source language: %s" % (args.f))
+
 originLines = []
 with open(args.f, 'r') as stringsFile:
     for line in stringsFile:
@@ -26,9 +29,9 @@ with open(args.f, 'r') as stringsFile:
             stringName = matchSource.group(1)
             sourceText = matchSource.group(3)
 
-            translationCouple = (stringName, sourceText)
+            translationTuple = (stringName, sourceText)
 
-            originLines.append(translationCouple)
+            originLines.append(translationTuple)
         else:
             if args.v == '1':
                 print("   ignoring source line: %s" % (line))
@@ -37,66 +40,124 @@ with open(args.f, 'r') as stringsFile:
     # end for
 # end with
 
-def createDirectoryIfNotExists(fileName):
+print("Total lines in source: %s\n" % (len(originLines)))
+
+def createOutputDirectoryIfNotExists(fileName):
     if not os.path.exists(os.path.dirname(fileName)):
         os.makedirs(os.path.dirname(fileName))
     #end if
 #end def
 
-def writeToFile(sourceText, translatedText, target):
-    fileName = "output/" + target + ".lproj/Localizable.strings"
+def writeToFile(sourceText, translatedText, outputTargetCode):
+    fileName = "output/" + outputTargetCode + ".lproj/Localizable.strings"
     with open(fileName, "a") as myfile:
-        createDirectoryIfNotExists(fileName)
+        createOutputDirectoryIfNotExists(fileName)
         contentToWrite = "\"" + sourceText + "\" = \"" + translatedText + "\";\n"
         myfile.write(contentToWrite)
     #end with
 #end def
 
-def translateSourceText(sourceText, target):
+def translateSourceText(sourceText, translateTargetCode):
+    """
+    Return translation for source text
+    :param sourceText: source text to translate
+    :param translateTargetCode: target language (google translation code)
+    :return:
+    """
     try:
-        obj = translator.translate(sourceText, src=args.o, dest=target)
+        obj = translator.translate(sourceText, src=args.o, dest=translateTargetCode)
     except Exception as e:
-        print("   translation failed for: %s = %s" % (sourceText, e))
-        return sourceText   
-    return obj.text
+        if args.v == '1':
+            print("   FAILED to translate for %s: %s = %s" % (translateTargetCode, sourceText, e))
+        #endif
+        return (sourceText, False)
+    #end try
+
+    return (obj.text, True)
 #end def
 
-def translateLineInFile(translationCouple, target, outputTarget):
-    stringName = translationCouple[0]
-    sourceText = translationCouple[1]
+def translationNeeded(translationTuple, translateTargetCode, outputTargetCode):
+    """
+    Check if translation is required for a given source key. If not delta-translating, this will always return True
+    other wise False if translation key found in original target Localizable.strings file.
 
-    translation = translateSourceText(sourceText, target)
-    writeToFile(stringName, translation, outputTarget)
+    :param translationTuple: key / value
+    :param translateTargetCode:  target language
+    :param outputTargetCode:  traget output file
+    :return: True if needed, False if translation was found in the original target.
+    """
+    stringName = translationTuple[0]
+    sourceText = translationTuple[1]
+
+    return True
+#end def
+
+def translateLineInFile(translationTuple, translateTargetCode, outputTargetCode):
+    """
+    Translate a given key / value tuple to a target language.
+
+    :param translationTuple: key / value
+    :param translateTargetCode:  target language
+    :param outputTargetCode:  output target code
+    """
+    stringName = translationTuple[0]
+    sourceText = translationTuple[1]
+
+    (translation, success) = translateSourceText(sourceText, translateTargetCode)
+
+    writeToFile(stringName, translation, outputTargetCode)
+
+    return success
 #end def
 
 def clearContentsOfFile(target):
     fileName = "output/" + target + ".lproj/Localizable.strings"
-    createDirectoryIfNotExists(fileName)
+    createOutputDirectoryIfNotExists(fileName)
     open(fileName, 'w').close()
 #end def
 
-def translateFile(translateName, target, outputTarget):
-    print("Translating for: " + translateName)
+def translateFile(translateFriendlyName, translateTargetCode, outputTargetCode):
+    """
+    Translate source language for the given target language / output file
+    :param translateFriendlyName: friendly name for printing
+    :param translateTargetCode: google translation target code
+    :param outputTargetCode: output target code
+    :return:
+    """
+    print("Translating for: " + translateFriendlyName)
 
-    clearContentsOfFile(outputTarget)
+    clearContentsOfFile(outputTargetCode)
 
-    for translationCouple in originLines:
-        translateLineInFile(translationCouple, translateTarget, outputTarget)
+    totalLinesTranslated = 0
+    totalLinesNeeded = 0
+    for translationTuple in originLines:
+        if translationNeeded(translationTuple, translateTargetCode, outputTargetCode):
+            totalLinesNeeded += 1
+
+            if translateLineInFile(translationTuple, translateTargetCode, outputTargetCode):
+                totalLinesTranslated += 1
+            #end if
+        #end if
     #end for
+
+    if totalLinesNeeded != totalLinesTranslated:
+        print("    WARNING: Total lines translated: %s. Original source count: %s" % (totalLinesTranslated, totalLinesNeeded))
+    #end if
 #end def
 
+with open('LanguageCodes.txt', 'r') as supportedLangCodeFile:
+    for targetLine in supportedLangCodeFile:
 
-with open('LanguageCodes.txt', 'r') as targetsFile:
-    for targetLine in targetsFile:
-
-        if "#" in targetLine:
+        if targetLine.strip().startswith("#"):
             continue
-        targetArray = targetLine.split()
-        translateName = targetArray[0]
-        translateTarget = targetArray[1]
-        outputTarget = targetArray[2]
+        #end if
 
-        translateFile(translateName, translateTarget, outputTarget)
+        targetArray = targetLine.split()
+        translateFriendlyName = targetArray[0]
+        translateTargetCode = targetArray[1]
+        outputTargetCode = targetArray[2]
+
+        translateFile(translateFriendlyName, translateTargetCode, outputTargetCode)
     #end for
 #end def
 
