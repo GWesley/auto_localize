18e3166aa70b77875a2f8ce3e04bc6750e2b181a
Fahad <fahad@guidedways.com>
2021-11-07 12:34:23 +0400

    googletrans no longer works, updated readme Read source language once, ignore unwanted lines Verbose printing Cleanup

--- README.md
+++ README.md
@@ -2,12 +2,14 @@
 Auto translate Localizable.strings for multiple languages in Xcode
 
 # Usage
-1. put your origin Localizable.strings file in folder
-2. pip3 install googletrans
-3. python3 strings.py
+1. put your origin `Localizable.strings` file in folder
+2. `pip3 install googletrans==4.0.0-rc1`
+3. `python3 strings.py`
 
 ![image](https://user-images.githubusercontent.com/2985638/133636085-7e3b7c1b-efcc-430a-a478-383ddd9e634f.png)
 
+# how to specify custom path to Localizable.strings
+`python3 strings.py -f /some/path/to/Localizable.strings`
 
 # how to set origin languge
 you can use `-o` to set your origin language,
@@ -17,5 +19,8 @@ for example `python3 strings.py -o fr`,
 the default origin is english
 
 # how to add more language
-update LanguageCodes.txt to add or remove support languages
+update `LanguageCodes.txt` to add or remove support languages
+
+# how to enable verbose printing
+`python3 strings.py -v`
 
--- strings.py
+++ strings.py
@@ -1,5 +1,4 @@
 import re
-import requests
 import os
 import argparse
 from googletrans import Translator
@@ -7,12 +6,42 @@ from googletrans import Translator
 translator = Translator()
 
 parser = argparse.ArgumentParser()
+parser.add_argument("-f", default="Localizable.strings", help="set the path to the original Localizable.strings to read keys from")
 parser.add_argument("-o", default="en", help="set the origin locale for auto translation, default is english")
+parser.add_argument("-v", default="1", help="Verbose")
 args = parser.parse_args()
+
+# Read and cache origin language once
+originLines = []
+with open(args.f, 'r') as stringsFile:
+    for line in stringsFile:
+        # Ignore empty lines
+        if len(line.strip()) == 0:
+            continue
+        # endif
+
+        # Ignore lines we cannot translate
+        matchSource = re.search(r'\"(.*)\"(.*)\"(.*)\"', line)
+        if matchSource:
+            stringName = matchSource.group(1)
+            sourceText = matchSource.group(3)
+
+            translationCouple = (stringName, sourceText)
+
+            originLines.append(translationCouple)
+        else:
+            if args.v == '1':
+                print("   ignoring source line: %s" % (line))
+            #end if
+        #end if
+    # end for
+# end with
 
 def createDirectoryIfNotExists(fileName):
     if not os.path.exists(os.path.dirname(fileName)):
         os.makedirs(os.path.dirname(fileName))
+    #end if
+#end def
 
 def writeToFile(sourceText, translatedText, target):
     fileName = "output/" + target + ".lproj/Localizable.strings"
@@ -20,36 +49,41 @@ def writeToFile(sourceText, translatedText, target):
         createDirectoryIfNotExists(fileName)
         contentToWrite = "\"" + sourceText + "\" = \"" + translatedText + "\";\n"
         myfile.write(contentToWrite)
+    #end with
+#end def
 
 def translateSourceText(sourceText, target):
     try:
         obj = translator.translate(sourceText, src=args.o, dest=target)
-    except:
+    except Exception as e:
+        print("   translation failed for: %s = %s" % (sourceText, e))
         return sourceText   
     return obj.text
+#end def
 
-def translateLineInFile(line, target, outputTarget):
-    matchSource = re.search(r'\"(.*)\"(.*)\"(.*)\"', line)
-    if matchSource:
-        stringName = matchSource.group(1)
-        sourceText = matchSource.group(3)
-        translation = translateSourceText(sourceText, target)
-        writeToFile(stringName, translation, outputTarget)
+def translateLineInFile(translationCouple, target, outputTarget):
+    stringName = translationCouple[0]
+    sourceText = translationCouple[1]
+
+    translation = translateSourceText(sourceText, target)
+    writeToFile(stringName, translation, outputTarget)
+#end def
 
 def clearContentsOfFile(target):
     fileName = "output/" + target + ".lproj/Localizable.strings"
     createDirectoryIfNotExists(fileName)
     open(fileName, 'w').close()
+#end def
 
 def translateFile(translateName, target, outputTarget):
-
     print("Translating for: " + translateName)
 
     clearContentsOfFile(outputTarget)
 
-    with open('Localizable.strings', 'r') as stringsFile:
-        for line in stringsFile:
-            translateLineInFile(line, translateTarget, outputTarget)
+    for translationCouple in originLines:
+        translateLineInFile(translationCouple, translateTarget, outputTarget)
+    #end for
+#end def
 
 
 with open('LanguageCodes.txt', 'r') as targetsFile:
@@ -63,5 +97,7 @@ with open('LanguageCodes.txt', 'r') as targetsFile:
         outputTarget = targetArray[2]
 
         translateFile(translateName, translateTarget, outputTarget)
+    #end for
+#end def
 
 
